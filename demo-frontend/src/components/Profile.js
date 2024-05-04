1e606b2c0168cf34c8c6d4c0552b35a950366c34
irina-bl <irina.blaj@alumnos.upm.es>
2024-05-04 17:43:31 +0200

    css profile

--- demo-frontend/src/components/Profile.js
+++ demo-frontend/src/components/Profile.js
@@ -1,27 +1,26 @@
 import React from "react";
 import AuthService from "../services/auth.service";
+import "./css/Profile.css"; // Importa el archivo CSS para estilos
 
 const Profile = () => {
   const currentUser = AuthService.getCurrentUser();
 
   return (
-    <div className="container">
-      <header className="jumbotron">
-        <h3>
-          <strong>{currentUser.username}</strong> Profile
-        </h3>
+    <div className="profile-container">
+      <header className="profile-header">
+        <h3>{currentUser.username} Profile</h3>
       </header>
-      <p>
-        <strong>Id:</strong> {currentUser.id}
-      </p>
-      <p>
-        <strong>Email:</strong> {currentUser.email}
-      </p>
-      <strong>Authorities:</strong>
-      <ul>
-        {currentUser.roles &&
-          currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
-      </ul>
+      <div className="profile-info">
+        <p><strong>Id:</strong> {currentUser.id}</p>
+        <p><strong>Email:</strong> {currentUser.email}</p>
+        <div>
+          <strong>Authorities:</strong>
+          <ul>
+            {currentUser.roles &&
+              currentUser.roles.map((role, index) => <li key={index}>{role}</li>)}
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
