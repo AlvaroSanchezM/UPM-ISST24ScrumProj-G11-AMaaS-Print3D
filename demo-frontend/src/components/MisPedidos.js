d301cea8e25eac7ce11c4e067eb0a723c4d20c28
irina-bl <irina.blaj@alumnos.upm.es>
2024-05-04 18:16:02 +0200

    css mispedidos

--- demo-frontend/src/components/MisPedidos.js
+++ demo-frontend/src/components/MisPedidos.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import OrderDetailsModal from './OrderDetailsModal';
+import './css/MisPedidos.css';
 
 export default function MisPedidos() {
     const [uploads, setUploads] = useState([]);
@@ -31,11 +32,11 @@ export default function MisPedidos() {
     };
 
     return (
-        <div className="container mt-3">
+        <div className="container mt-3 container-mis-pedidos">
             <h2 className="mb-4">Mis Pedidos</h2>
             {uploads.length > 0 ? (
                 uploads.map((upload, index) => (
-                    <div key={index} className="card mb-3">
+                    <div key={index} className="card mb-3 card-mis-pedidos">
                         <div className="card-body">
                             <h5 className="card-title">Order {index + 1}</h5>
                             <button className="btn btn-primary" onClick={() => handleViewDetails(upload.id)}>More Info</button>
