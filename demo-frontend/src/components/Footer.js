29e6dd3b533f612f0af5026bdc69801af7001b4b
silviaroher <silvia.rodriguezh@alumnos.upm.es>
2024-05-05 23:45:29 +0200

    Footer

---
+++ demo-frontend/src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './css/Footer.css';  // Asume que tienes un archivo CSS para estilos
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div className="container">
+                <p>© 2024 Print3D. Todos los derechos reservados</p>
+                <p>Contacto: print3d@gmail.com</p>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
